8c037fbcb7f31922164fe8b5a5d1954af209c69a
asukii314 <82646644+asukii314@users.noreply.github.com>
2025-09-10 01:14:54 -0400

    Save twitch IDs for finalists

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const TWITCH_IDS = {
+  vmService: 59817220,
+  TerrenceMHS: 26480150
+};
+    
 async function fetchGameData() {
-  const url = 'https://slay-the-relics.baalorlord.tv/api/v2/game-state/601604206';
+  const url = 'https://slay-the-relics.baalorlord.tv/api/v2/game-state/601604206'; // static placeholder
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json(); // Or .text() for plain text
-    console.log(data);
+    const data = await response.json();
+    console.log(data); // do something with the data!
+    
   } catch (error) {
     console.error("Error fetching data:", error);
   }
